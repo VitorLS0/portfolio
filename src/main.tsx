011a2dd6d7cfd456fbc90dce6a2c74246e1e0233
VitorLS0 <vitorlberg@gmail.com>
2026-09-15 20:13:16 -0300

    fix css import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+// Ignore missing type declarations for CSS side-effect import
+// @ts-ignore
+import './index.css'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
