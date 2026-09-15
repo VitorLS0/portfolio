d4a048ab7b5ba59e2ac73e4a4ae33fc2f33ae693
VitorLS0 <vitorlberg@gmail.com>
2026-09-15 19:37:47 -0300

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { Analytics } from "@vercel/analytics/react";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
-)
+);
